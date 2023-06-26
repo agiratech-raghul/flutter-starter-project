5e9e085db9551c5019cda7c0653357dbe1bb4258
raghul <agira@Agiras-MacBook-Pro.local>
2023-06-26 18:39:04 +0530

    zone mismatched error fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
 import 'dart:async';
 
 import 'src/app.dart';
 import 'src/services/services_initializer.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
   final ProviderContainer container = ProviderContainer();
-  await container.read(servicesInitializerProvider).init();
-
-  await SentryFlutter.init(
-        (options) {
-          options.dsn = 'https://f349d25ceb6d4417aaeaef1b88ac4e56@o4505425760092160.ingest.sentry.io/4505425765597184';
-          options.tracesSampleRate = 1.0;
-    },
-    appRunner: () => runApp(UncontrolledProviderScope(container: container, child: const App()))
-  );
+  runZonedGuarded<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await container.read(servicesInitializerProvider).init();
+    runApp(UncontrolledProviderScope(container: container, child: const App()));
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    FlutterError.onError = (FlutterErrorDetails details) {
+      FlutterError.presentError(details);
+    };
+    ErrorWidget.builder = (FlutterErrorDetails details) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: const Text('An error occurred'),
+        ),
+        body: Center(child: Text(details.toString())),
+      );
+    };
+  }, (Object error, StackTrace stack) {
+    debugPrint(error.toString());
+  });
 }
